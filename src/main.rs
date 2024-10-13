a65c799196384c7048521feec6a56ccd3748f3f0
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2024-10-13 17:56:22 +0330

    :tada: new ohmydb used

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,46 @@
 #![allow(unused_variables)]
 
-use ohmydb::{fake_it, JsonDB, ToDo};
+use ohmydb::JsonDB;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default, PartialEq, Eq, Hash)]
+struct ToDo {
+    id: String,
+    title: String,
+    status: String,
+    point: u32,
+    name: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let mut db = JsonDB::new().await.unwrap();
+    let mut db = JsonDB::new("").await.unwrap();
 
-    db.add_table("product").unwrap();
+    db.add_table("product").await.unwrap();
 
-    let todos = fake_it::<ToDo>(1);
+    let todos = vec![
+        ToDo {
+            id: "1".to_string(),
+            title: "Buy milk".to_string(),
+            status: "Pending".to_string(),
+            point: 100,
+            name: "John".to_string(),
+        },
+        ToDo {
+            id: "2".to_string(),
+            title: "Buy bread".to_string(),
+            status: "Pending".to_string(),
+            point: 200,
+            name: "John".to_string(),
+        },
+        ToDo {
+            id: "3".to_string(),
+            title: "Buy eggs".to_string(),
+            status: "Pending".to_string(),
+            point: 300,
+            name: "John".to_string(),
+        },
+    ];
 
     for todo in &todos {
         db.insert_or("person", todo.clone()).run().await.unwrap();
@@ -43,17 +75,17 @@ async fn main() {
     println!("{:#?}", found);
 
     println!("************\nDeleted:\n************\n");
-    let deleted = db
-        .delete("person")
-        .where_("status")
-        .not_equals("Archived")
-        .where_("point")
-        .less_than(500)
-        .run()
-        .await
-        .unwrap();
+    // let deleted = db
+    //     .delete("person")
+    //     .where_("status")
+    //     .not_equals("Archived")
+    //     .where_("point")
+    //     .less_than(500)
+    //     .run()
+    //     .await
+    //     .unwrap();
 
-    println!("{:#?}", deleted);
+    // println!("{:#?}", deleted);
 
     println!("************\nUpdate:\n************\n");
     let td = ToDo::default();
